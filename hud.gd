a2767b555421f21ff17cee0b671eb23fa3f4a8f4
David Moulin <moulin.david@live.fr>
2025-04-21 20:12:59 +0200

    feat: hud

--- hud.gd
+++ hud.gd
@@ -17,7 +17,6 @@ func show_message(text):
 func show_game_over():
 	show_message("Game Over")
 	await $MessageTimer.timeout
-
 	$Message.text = "Dodge the Creeps!"
 	$Message.show()
 	await get_tree().create_timer(1.0).timeout
